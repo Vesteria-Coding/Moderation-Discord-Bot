bf621e717a9fe9f23719977436b949cb05839884
Thalia Blair <holden.blairpeace17@gmail.com>
2025-10-26 14:17:48 -0400

    Fix indentation and formatting in Bot.py

--- Bot.py
+++ Bot.py
@@ -10,7 +10,6 @@ from discord import app_commands, Interaction, Embed
 
 # Setup Credentials
 load_dotenv()
-MOD_API = os.getenv("MOD_API")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GUILD_ID = int(os.getenv("GUILD_ID"))
 PASTEBIN_KEY = os.getenv("PASTEBIN_KEY")
